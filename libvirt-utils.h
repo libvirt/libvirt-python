c6ff569fb6a535cb625cd99d22052f756eae5b56
Doug Goldstein <cardoe@cardoe.com>
2013-12-29 16:52:15 -0600

    define __GNUC_PREREQ macro before using it
    We brought over use of the __GNUC_PREREQ macro from libvirt but didn't
    bring over the definition of it. This brings over the macro from libvirt
    sources.

--- libvirt-utils.h
+++ libvirt-utils.h
@@ -65,6 +65,16 @@
 # endif
 
 # ifdef __GNUC__
+
+#  ifndef __GNUC_PREREQ
+#   if defined __GNUC__ && defined __GNUC_MINOR__
+#    define __GNUC_PREREQ(maj, min)                                        \
+    ((__GNUC__ << 16) + __GNUC_MINOR__ >= ((maj) << 16) + (min))
+#   else
+#    define __GNUC_PREREQ(maj,min) 0
+#   endif
+#  endif /* __GNUC_PREREQ */
+
 /**
  * ATTRIBUTE_UNUSED:
  *
