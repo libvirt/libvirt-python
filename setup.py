53a5e24aee36bc574b1f8451760059fd06abc9d2
Daniel Veillard <veillard@redhat.com>
2016-01-17 10:49:23 +0800

    Version bump to 1.3.1
    For release but no change from 1.3.0

--- setup.py
+++ setup.py
@@ -311,7 +311,7 @@ class my_clean(clean):
 _c_modules, _py_modules = get_module_lists()
 
 setup(name = 'libvirt-python',
-      version = '1.3.0',
+      version = '1.3.1',
       url = 'http://www.libvirt.org',
       maintainer = 'Libvirt Maintainers',
       maintainer_email = 'libvir-list@redhat.com',
