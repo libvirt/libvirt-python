bc4c7477f02f3085bf2be47998b17fa6589823ab
Pavel Hrdina <phrdina@redhat.com>
2016-02-23 13:45:36 +0100

    libvirt-override: fix PyArg_ParseTuple for unsigned long long
    Format string uses 'L' for long long type and 'K' for unsigned long long
    type.
    
    Resolves: https://bugzilla.redhat.com/show_bug.cgi?id=1260356
    
    Signed-off-by: Pavel Hrdina <phrdina@redhat.com>

--- libvirt-override.c
+++ libvirt-override.c
@@ -7552,7 +7552,7 @@ libvirt_virDomainBlockPeek(PyObject *self ATTRIBUTE_UNUSED,
     char *buf;
     unsigned int flags;
 
-    if (!PyArg_ParseTuple(args, (char *)"OzLnI:virDomainBlockPeek",
+    if (!PyArg_ParseTuple(args, (char *)"OzKnI:virDomainBlockPeek",
                           &pyobj_domain, &disk, &offset, &size, &flags))
         return NULL;
 
@@ -7590,7 +7590,7 @@ libvirt_virDomainMemoryPeek(PyObject *self ATTRIBUTE_UNUSED,
     char *buf;
     unsigned int flags;
 
-    if (!PyArg_ParseTuple(args, (char *)"OLnI:virDomainMemoryPeek",
+    if (!PyArg_ParseTuple(args, (char *)"OKnI:virDomainMemoryPeek",
                           &pyobj_domain, &start, &size, &flags))
         return NULL;
 
