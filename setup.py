3e041e5b36b27f047323e9c07989f60a79780266
Peter Krempa <pkrempa@redhat.com>
2016-09-19 17:19:32 +0200

    Post-release version bump to 2.3.0

--- setup.py
+++ setup.py
@@ -307,7 +307,7 @@ class my_clean(clean):
 _c_modules, _py_modules = get_module_lists()
 
 setup(name = 'libvirt-python',
-      version = '2.2.0',
+      version = '2.3.0',
       url = 'http://www.libvirt.org',
       maintainer = 'Libvirt Maintainers',
       maintainer_email = 'libvir-list@redhat.com',
