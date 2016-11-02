653ca92463788c0f1873f53b81c6d5f5a37784c1
Pavel Hrdina <phrdina@redhat.com>
2016-11-02 12:42:12 +0100

    Post-release version bump to 2.5.0
    Signed-off-by: Pavel Hrdina <phrdina@redhat.com>

--- setup.py
+++ setup.py
@@ -307,7 +307,7 @@ class my_clean(clean):
 _c_modules, _py_modules = get_module_lists()
 
 setup(name = 'libvirt-python',
-      version = '2.4.0',
+      version = '2.5.0',
       url = 'http://www.libvirt.org',
       maintainer = 'Libvirt Maintainers',
       maintainer_email = 'libvir-list@redhat.com',
