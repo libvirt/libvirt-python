libvirt/libvirt-python

commit 1d39dbaf637db03f6e597ed56b96aa065710b4a1

use VYR_PY_LIST_SET_GOTO and VIR_PY_LIST_APPEND_GOTO
Signed-off-by: Pavel Hrdina <phrdina@redhat.com>

libvirt-lxc-override.c
@@ -82,17 +82,8 @@ libvirt_lxc_virDomainLxcOpenNamespace(PyObject *self ATTRIBUTE_UNUSED,
     if ((py_retval = PyList_New(0)) == NULL)
         goto error;
 
-    for (i = 0; i < c_retval; i++) {
-        PyObject *item = NULL;
-
-        if ((item = libvirt_intWrap(fdlist[i])) == NULL)
-            goto error;
-
-        if (PyList_Append(py_retval, item) < 0) {
-            Py_DECREF(item);
-            goto error;
-        }
-    }
+    for (i = 0; i < c_retval; i++)
+        VIR_PY_LIST_APPEND_GOTO(py_retval, libvirt_intWrap(fdlist[1]), error);
 
  cleanup:
     VIR_FREE(fdlist);

commit 6798609a5ec5ba6a0d17633432591dacbc6d7f08

Fix regression in lxcOpenNamespace
This fixes regression caused by the 1d39dbaf637db03f6e597ed56b96aa065710b4a1

fdlist[i] erroneously was replaced by fdlist[1] which caused
lxcOpenNamespace to return a list with identical elements.

Signed-off-by: Sergei Turchanov <turchanov@farpost.com>
Reviewed-by: Ján Tomko <jtomko@redhat.com>
Signed-off-by: Ján Tomko <jtomko@redhat.com>

libvirt-lxc-override.c
@@ -83,7 +83,7 @@ libvirt_lxc_virDomainLxcOpenNamespace(PyObject *self ATTRIBUTE_UNUSED,
         goto error;
 
     for (i = 0; i < c_retval; i++)
-        VIR_PY_LIST_APPEND_GOTO(py_retval, libvirt_intWrap(fdlist[1]), error);
+        VIR_PY_LIST_APPEND_GOTO(py_retval, libvirt_intWrap(fdlist[i]), error);
 
  cleanup:
     VIR_FREE(fdlist);