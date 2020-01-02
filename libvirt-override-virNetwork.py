7b18e3bae8b4abec99fea2db639a6e49555c76b9
Daniel P. Berrangé <berrange@redhat.com>
2020-01-02 15:34:14 +0000

    fix class type instantiated when listing network ports
    Reviewed-by: Erik Skultety <eskultet@redhat.com>
    Signed-off-by: Daniel P. Berrangé <berrange@redhat.com>

--- libvirt-override-virNetwork.py
+++ libvirt-override-virNetwork.py
@@ -1,11 +1,11 @@
     def listAllPorts(self, flags=0):
-        """List all domains and returns a list of domain objects"""
+        """List all ports on the network and returns a list of network port objects"""
         ret = libvirtmod.virNetworkListAllPorts(self._o, flags)
         if ret is None:
             raise libvirtError("virNetworkListAllPorts() failed", conn=self)
 
         retlist = list()
         for domptr in ret:
-            retlist.append(virNetwork(self, _obj=domptr))
+            retlist.append(virNetworkPort(self, _obj=domptr))
 
         return retlist
