libvirt/libvirt-python

commit fd069ac85c8cf1593587dc9287a3d5eb6bd4bdb9

Fix wrong type for BlockThreshold Callback
type of threshold and excess are unsigned long long,
but PyObject_CallMethod incorrectly set it to int.

if threshold or excess greater than 0x7FFFFFFF(max int),
those variables will overflow.

Signed-off-by: Ren Lei <ren.lei4@zte.com.cn>

libvirt-override.c
@@ -7232,7 +7232,7 @@ libvirt_virConnectDomainEventBlockThresholdCallback(virConnectPtr conn ATTRIBUTE
     /* Call the Callback Dispatcher */
     pyobj_ret = PyObject_CallMethod(pyobj_conn,
                                     (char*)"_dispatchDomainEventBlockThresholdCallback",
-                                    (char*)"OssiiO",
+                                    (char*)"OssLLO",
                                     pyobj_dom, dev, path, threshold, excess,
                                     pyobj_cbData);
 

commit b5cd547bfd7e9dd566de672f76a17aa9229b7c52

Fix BlockThreshold Callback argument conversion once more
The conversion was changed from "OssiiO" to "OssLLO". Unfortunately the
arguments are unsigned long long, where the proper coversion character
is 'K'.

Fixes: https://gitlab.com/libvirt/libvirt-python/-/merge_requests/40
Fixes: fd069ac85c8cf1593587dc9287a3d5eb6bd4bdb9
Resolves: https://bugzilla.redhat.com/show_bug.cgi?id=1976109
Signed-off-by: Peter Krempa <pkrempa@redhat.com>

libvirt-override.c
@@ -7236,7 +7236,7 @@ libvirt_virConnectDomainEventBlockThresholdCallback(virConnectPtr conn ATTRIBUTE
     /* Call the Callback Dispatcher */
     pyobj_ret = PyObject_CallMethod(pyobj_conn,
                                     (char*)"_dispatchDomainEventBlockThresholdCallback",
-                                    (char*)"OssLLO",
+                                    (char*)"OssKKO",
                                     pyobj_dom, dev, path, threshold, excess,
                                     pyobj_cbData);
 