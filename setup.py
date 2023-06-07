90bf275b7db6c41cfaa4efe22c97a3f496f4aade
Erik Skultety <eskultet@redhat.com>
2023-06-07 16:43:51 +0200

    setup: Reduce the Python classifiers to "Python :: 3" only
    We're not good at tracking all the released Python 3 versions (+ we
    don't support Python 2 anymore).
    Additionally, future patches will take care of reporting the minimum
    required version of Python anyway through the 'python_requires' field
    which shows up correctly in PyPI.
    
    Signed-off-by: Erik Skultety <eskultet@redhat.com>

--- setup.py
+++ setup.py
@@ -362,13 +362,6 @@ of recent versions of Linux (and other OSes).''',
           "Development Status :: 5 - Production/Stable",
           "Intended Audience :: Developers",
           "License :: OSI Approved :: GNU Lesser General Public License v2 or later (LGPLv2+)",
-          "Programming Language :: Python",
           "Programming Language :: Python :: 3",
-          "Programming Language :: Python :: 3.5",
-          "Programming Language :: Python :: 3.6",
-          "Programming Language :: Python :: 3.7",
-          "Programming Language :: Python :: 3.8",
-          "Programming Language :: Python :: 3.9",
-          "Programming Language :: Python :: 3.10",
       ]
 )
