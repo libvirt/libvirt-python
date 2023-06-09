libvirt/libvirt-python

commit e749b4cf111bcfc9ceeb88be57dc1d810fc631d9

setup: Move away from os.path to pathlib.Path
Drops usage of the glob module along the way as pathlib integrates glob
as well.

Signed-off-by: Erik Skultety <eskultet@redhat.com>

setup.py
@@ -1,14 +1,13 @@
 #!/usr/bin/env python3
 
-import glob
 import sys
 import os
-import os.path
 import re
 import shutil
 import subprocess
 import time
 
+from pathlib import Path
 from setuptools import setup, Extension, Command
 from setuptools.command.build_ext import build_ext
 from setuptools.command.build_py import build_py
@@ -229,10 +228,9 @@ class my_sdist(sdist):
         f2.close()
 
     def run(self):
-        if not os.path.exists("build"):
-            os.mkdir("build")
+        Path("build").mkdir(exist_ok=True)
 
-        if os.path.exists(".git"):
+        if Path(".git").exists():
             try:
                 self.gen_rpm_spec()
                 self.gen_authors()
@@ -247,8 +245,10 @@ class my_sdist(sdist):
                     "ChangeLog"
                 ]
                 for f in files:
-                    if os.path.exists(f):
-                        os.unlink(f)
+                    try:
+                        Path(f).unlink()
+                    except FileNotFoundError:
+                        pass
         else:
             sdist.run(self)
 
@@ -269,11 +269,12 @@ class my_test(Command):
         if self.plat_name is not None:
             plat_specifier = f".{self.plat_name}-{sys.version_info[0]}.{sys.version_info[1]}"
 
-            return os.path.join(self.build_base,
-                                'lib' + plat_specifier)
+            if hasattr(sys, "gettotalrefcount"):
+                plat_specifier += "-pydebug"
+
+            return Path(self.build_base, "lib" + plat_specifier).as_posix()
         else:
-            dirs = glob.glob(os.path.join(self.build_base,
-                                          "lib.*"))
+            dirs = [p.as_posix() for p in Path(self.build_base).glob("lib.*")]
             if len(dirs) == 0:
                 print("No build directory found, run 'setup.py build'")
                 sys.exit(1)

commit 3ffb60b7c8bce3fad4a2c284e0c68321574ba5e9

setup: styling: Fix last occurrence of os.path in favour of pathlib
Commit e749b4cf forgot to update a couple more occurrences.

Fixes: e749b4cf111bcfc9ceeb88be57dc1d810fc631d9
Signed-off-by: Erik Skultety <eskultet@redhat.com>

setup.py
@@ -338,7 +338,7 @@ class my_clean(Command):
         pass
 
     def run(self):
-        if os.path.exists("build"):
+        if Path("build").exists():
             shutil.rmtree("build", ignore_errors=True)
 
 
@@ -355,8 +355,7 @@ MIN_LIBVIRT_LXC = "1.0.2"
 
 # Hack to stop "pip install" failing with error
 # about missing "build" dir.
-if not os.path.exists("build"):
-    os.mkdir("build")
+Path("build").mkdir(exist_ok=True)
 
 check_pkgcfg()
 