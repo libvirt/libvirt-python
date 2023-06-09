libvirt/libvirt-python

commit 745e5a8ca46feb1acf3382c38eb45c74c1c42489

setup: styling: Adopt f-strings in favour of old-style string formatting
Signed-off-by: Erik Skultety <eskultet@redhat.com>

setup.py
@@ -59,7 +59,9 @@ def have_libvirt_lxc():
 
 def get_pkgconfig_data(args, mod, required=True):
     """Run pkg-config to and return content associated with it"""
-    f = os.popen("%s %s %s" % ("pkg-config", " ".join(args), mod))
+
+    args_str = " ".join(args)
+    f = os.popen(f"pkg-config {args_str} {mod}")
 
     line = f.readline()
     if line is not None:
@@ -67,7 +69,7 @@ def get_pkgconfig_data(args, mod, required=True):
 
     if line is None or line == "":
         if required:
-            raise Exception("Cannot determine '%s' from libvirt pkg-config file" % " ".join(args))
+            raise Exception(f"Cannot determine '{args_str}' from libvirt pkg-config file")
         else:
             return ""
 
@@ -261,11 +263,7 @@ class my_test(Command):
 
     def find_build_dir(self):
         if self.plat_name is not None:
-            plat_specifier = ".%s-%d.%d" % (self.plat_name,
-                                            sys.version_info[0],
-                                            sys.version_info[1])
-            if hasattr(sys, "gettotalrefcount"):
-                plat_specifier += "-pydebug"
+            plat_specifier = f".{self.plat_name}-{sys.version_info[0]}.{sys.version_info[1]}"
 
             return os.path.join(self.build_base,
                                 'lib' + plat_specifier)
@@ -291,10 +289,9 @@ class my_test(Command):
 
     def find_pytest_path(self):
         binaries = [
-            "pytest-%d.%d" % (sys.version_info[0],
-                                 sys.version_info[1]),
-            "pytest-%d" % (sys.version_info[0]),
-            "pytest%d" % (sys.version_info[0]),
+            f"pytest-{sys.version_info[0]}.{sys.version_info[1]}",
+            f"pytest-{sys.version_info[0]}",
+            f"pytest{sys.version_info[0]}",
             "pytest",
         ]
 

commit 583203c94c277fb712116dbc5691bac59d2f9233

setup: styling: More f-string conversions
Fix a few more occurrences.

Fixes: 745e5a8ca46feb1acf3382c38eb45c74c1c42489
Signed-off-by: Erik Skultety <eskultet@redhat.com>

setup.py
@@ -30,13 +30,13 @@ def check_pkgcfg():
 def check_minimum_libvirt_version():
     subprocess.check_call(["pkg-config",
                            "--print-errors",
-                           "--atleast-version=%s" % MIN_LIBVIRT,
+                           f"--atleast-version={MIN_LIBVIRT}",
                            "libvirt"])
 
 
 def have_libvirt_lxc():
     proc = subprocess.run(["pkg-config",
-                           "--atleast-version=%s" % MIN_LIBVIRT_LXC,
+                           f"--atleast-version={MIN_LIBVIRT_LXC}",
                            "libvirt"])
     if proc.returncode == 0:
         return True