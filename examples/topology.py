libvirt/libvirt-python

commit 34aa32ba8a4d3373ce01f999021e58638c4c3324

Move python example programs into python/examples/ subdirectory
Signed-off-by: Daniel P. Berrange <berrange@redhat.com>

examples/topology.py
@@ -1,0 +1,45 @@
+#!/usr/bin/env python
+# Parse topology information from the capabilities XML and use
+# them to calculate host topology
+#
+# Authors:
+#   Amador Pahim <apahim@redhat.com>
+#   Peter Krempa <pkrempa@redhat.com>
+
+import libvirt
+import sys
+from xml.dom import minidom
+
+try:
+    conn = libvirt.openReadOnly(None)
+except libvirt.libvirtError:
+    print 'Failed to connect to the hypervisor'
+    sys.exit(1)
+
+try:
+    capsXML = conn.getCapabilities()
+except libvirt.libvirtError:
+    print 'Failed to request capabilities'
+    sys.exit(1)
+
+caps = minidom.parseString(capsXML)
+host = caps.getElementsByTagName('host')[0]
+cells = host.getElementsByTagName('cells')[0]
+total_cpus = cells.getElementsByTagName('cpu').length
+
+socketIds = []
+siblingsIds = []
+
+socketIds = [ proc.getAttribute('socket_id')
+              for proc in cells.getElementsByTagName('cpu')
+              if proc.getAttribute('socket_id') not in socketIds ]
+
+siblingsIds = [ proc.getAttribute('siblings')
+                for proc in cells.getElementsByTagName('cpu')
+                if proc.getAttribute('siblings') not in siblingsIds ]
+
+print "Host topology"
+print "NUMA nodes:", cells.getAttribute('num')
+print "   Sockets:", len(set(socketIds))
+print "     Cores:", len(set(siblingsIds))
+print "   Threads:", total_cpus

examples/Makefile.am
@@ -1,0 +1,21 @@
+## Copyright (C) 2005-2013 Red Hat, Inc.
+##
+## This library is free software; you can redistribute it and/or
+## modify it under the terms of the GNU Lesser General Public
+## License as published by the Free Software Foundation; either
+## version 2.1 of the License, or (at your option) any later version.
+##
+## This library is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+## Lesser General Public License for more details.
+##
+## You should have received a copy of the GNU Lesser General Public
+## License along with this library.  If not, see
+## <http://www.gnu.org/licenses/>.
+
+EXTRA_DIST=						\
+	README						\
+	consolecallback.py				\
+	topology.py					\
+	dominfo.py domrestore.py domsave.py domstart.py esxlist.py

examples/README
@@ -1,0 +1,33 @@
+Some simple examples on how to use the Python API for libvirt
+
+The examples are:
+
+dominfo.py  - print information about a running domU based on the results of
+              virDomainGetInfo and virDomainGetXMLDesc
+domstart.py - create a domU from an XML description if the domU isn't
+              running yet
+domsave.py  - save all running domU's into a directory
+domrestore.py - restore domU's from their saved files in a directory
+esxlist.py  - list active domains of an VMware ESX host and print some info.
+              also demonstrates how to use the libvirt.openAuth() method
+
+The XML files in this directory are examples of the XML format that libvirt
+expects, and will have to be adapted for your setup. They are only needed
+for domstart.py
+
+
+Some additional notes for the esxlist.py example:
+
+You may see remote errors complaining about missing certificates:
+
+  Cannot access CA certificate '/usr/local/etc/pki/CA/cacert.pem': No such file
+  or directory
+
+This is expected, libvirt tries to find network and storage drivers for ESX,
+but those are not implemented yet (November 2009). While searching for this
+drivers, libvirt may try to start a local libvirtd instance, but fails because
+of the missing certificates. It'll warn about that:
+
+  Failed to find the network: Is the daemon running?
+
+This is also expected and can be ignored.

examples/consolecallback.py
@@ -1,0 +1,88 @@
+#!/usr/bin/env python
+# consolecallback - provide a persistent console that survives guest reboots
+
+import sys, os, logging, libvirt, tty, termios, atexit
+
+def reset_term():
+    termios.tcsetattr(0, termios.TCSADRAIN, attrs)
+
+def error_handler(unused, error):
+    # The console stream errors on VM shutdown; we don't care
+    if (error[0] == libvirt.VIR_ERR_RPC and
+        error[1] == libvirt.VIR_FROM_STREAMS):
+        return
+    logging.warn(error)
+
+class Console(object):
+    def __init__(self, uri, uuid):
+        self.uri = uri
+        self.uuid = uuid
+        self.connection = libvirt.open(uri)
+        self.domain = self.connection.lookupByUUIDString(uuid)
+        self.state = self.domain.state(0)
+        self.connection.domainEventRegister(lifecycle_callback, self)
+        self.stream = None
+        self.run_console = True
+        logging.info("%s initial state %d, reason %d",
+                     self.uuid, self.state[0], self.state[1])
+
+def check_console(console):
+    if (console.state[0] == libvirt.VIR_DOMAIN_RUNNING or
+        console.state[0] == libvirt.VIR_DOMAIN_PAUSED):
+        if console.stream is None:
+            console.stream = console.connection.newStream(libvirt.VIR_STREAM_NONBLOCK)
+            console.domain.openConsole(None, console.stream, 0)
+            console.stream.eventAddCallback(libvirt.VIR_STREAM_EVENT_READABLE, stream_callback, console)
+    else:
+        if console.stream:
+            console.stream.eventRemoveCallback()
+            console.stream = None
+
+    return console.run_console
+
+def stdin_callback(watch, fd, events, console):
+    readbuf = os.read(fd, 1024)
+    if readbuf.startswith(""):
+        console.run_console = False
+        return
+    if console.stream:
+        console.stream.send(readbuf)
+
+def stream_callback(stream, events, console):
+    try:
+        received_data = console.stream.recv(1024)
+    except:
+        return
+    os.write(0, received_data)
+
+def lifecycle_callback (connection, domain, event, detail, console):
+    console.state = console.domain.state(0)
+    logging.info("%s transitioned to state %d, reason %d",
+                 console.uuid, console.state[0], console.state[1])
+
+# main
+if len(sys.argv) != 3:
+    print "Usage:", sys.argv[0], "URI UUID"
+    print "for example:", sys.argv[0], "'qemu:///system' '32ad945f-7e78-c33a-e96d-39f25e025d81'"
+    sys.exit(1)
+
+uri = sys.argv[1]
+uuid = sys.argv[2]
+
+print "Escape character is ^]"
+logging.basicConfig(filename='msg.log', level=logging.DEBUG)
+logging.info("URI: %s", uri)
+logging.info("UUID: %s", uuid)
+
+libvirt.virEventRegisterDefaultImpl()
+libvirt.registerErrorHandler(error_handler, None)
+
+atexit.register(reset_term)
+attrs = termios.tcgetattr(0)
+tty.setraw(0)
+
+console = Console(uri, uuid)
+console.stdin_watch = libvirt.virEventAddHandle(0, libvirt.VIR_EVENT_HANDLE_READABLE, stdin_callback, console)
+
+while check_console(console):
+    libvirt.virEventRunDefaultImpl()

examples/dominfo.py
@@ -1,0 +1,80 @@
+#!/usr/bin/env python
+# dominfo - print some information about a domain
+
+import libvirt
+import sys
+import os
+import libxml2
+import pdb
+
+def usage():
+   print 'Usage: %s DOMAIN' % sys.argv[0]
+   print '       Print information about the domain DOMAIN'
+
+def print_section(title):
+    print "\n%s" % title
+    print "=" * 60
+
+def print_entry(key, value):
+    print "%-10s %-10s" % (key, value)
+
+def print_xml(key, ctx, path):
+    res = ctx.xpathEval(path)
+    if res is None or len(res) == 0:
+        value="Unknown"
+    else:
+        value = res[0].content
+    print_entry(key, value)
+    return value
+
+if len(sys.argv) != 2:
+    usage()
+    sys.exit(2)
+
+name = sys.argv[1]
+
+# Connect to libvirt
+conn = libvirt.openReadOnly(None)
+if conn is None:
+    print 'Failed to open connection to the hypervisor'
+    sys.exit(1)
+
+try:
+    dom = conn.lookupByName(name)
+    # Annoyiingly, libvirt prints its own error message here
+except libvirt.libvirtError:
+    print "Domain %s is not running" % name
+    sys.exit(0)
+
+info = dom.info()
+print_section("Domain info")
+print_entry("State:", info[0])
+print_entry("MaxMem:", info[1])
+print_entry("UsedMem:", info[2])
+print_entry("VCPUs:", info[3])
+
+# Read some info from the XML desc
+xmldesc = dom.XMLDesc(0)
+doc = libxml2.parseDoc(xmldesc)
+ctx = doc.xpathNewContext()
+print_section("Kernel")
+print_xml("Type:", ctx, "/domain/os/type")
+print_xml("Kernel:", ctx, "/domain/os/kernel")
+print_xml("initrd:", ctx, "/domain/os/initrd")
+print_xml("cmdline:", ctx, "/domain/os/cmdline")
+
+print_section("Devices")
+devs = ctx.xpathEval("/domain/devices/*")
+for d in devs:
+    ctx.setContextNode(d)
+    #pdb.set_trace()
+    type = print_xml("Type:", ctx, "@type")
+    if type == "file":
+        print_xml("Source:", ctx, "source/@file")
+        print_xml("Target:", ctx, "target/@dev")
+    elif type == "block":
+        print_xml("Source:", ctx, "source/@dev")
+        print_xml("Target:", ctx, "target/@dev")
+    elif type == "bridge":
+        print_xml("Source:", ctx, "source/@bridge")
+        print_xml("MAC Addr:", ctx, "mac/@address")

examples/domrestore.py
@@ -1,0 +1,36 @@
+#!/usr/bin/env python
+# domstart - make sure a given domU is running, if not start it
+
+import libvirt
+import sys
+import os
+import libxml2
+import pdb
+
+def usage():
+   print 'Usage: %s DIR' % sys.argv[0]
+   print '       Restore all the domains contained in DIR'
+   print '       It is assumed that all files in DIR are'
+   print '       images of domU\'s previously created with save'
+
+if len(sys.argv) != 2:
+    usage()
+    sys.exit(2)
+
+dir = sys.argv[1]
+imgs = os.listdir(dir)
+
+conn = libvirt.open(None)
+if conn is None:
+    print 'Failed to open connection to the hypervisor'
+    sys.exit(1)
+
+for img in imgs:
+    file = os.path.join(dir, img)
+    print "Restoring %s ... " % img,
+    sys.stdout.flush()
+    ret = conn.restore(file)
+    if ret == 0:
+        print "done"
+    else:
+        print "error %d" % ret

examples/domsave.py
@@ -1,0 +1,40 @@
+#!/usr/bin/env python
+# domstart - make sure a given domU is running, if not start it
+
+import libvirt
+import sys
+import os
+import libxml2
+import pdb
+
+def usage():
+   print 'Usage: %s DIR' % sys.argv[0]
+   print '       Save all currently running domU\'s into DIR'
+   print '       DIR must exist and be writable by this process'
+
+if len(sys.argv) != 2:
+    usage()
+    sys.exit(2)
+
+dir = sys.argv[1]
+
+conn = libvirt.open(None)
+if conn is None:
+    print 'Failed to open connection to the hypervisor'
+    sys.exit(1)
+
+doms = conn.listDomainsID()
+for id in doms:
+    if id == 0:
+        continue
+    dom = conn.lookupByID(id)
+    print "Saving %s[%d] ... " % (dom.name(), id),
+    sys.stdout.flush()
+    path = os.path.join(dir, dom.name())
+    ret = dom.save(path)
+    if ret == 0:
+        print "done"
+    else:
+        print "error %d" % ret
+
+#pdb.set_trace()

examples/domstart.py
@@ -1,0 +1,50 @@
+#!/usr/bin/env python
+# domstart - make sure a given domU is running, if not start it
+
+import libvirt
+import sys
+import os
+import libxml2
+import pdb
+
+# Parse the XML description of domU from FNAME
+# and return a tuple (name, xmldesc) where NAME
+# is the name of the domain, and xmldesc is the contetn of FNAME
+def read_domain(fname):
+    fp = open(fname, "r")
+    xmldesc = fp.read()
+    fp.close()
+
+    doc = libxml2.parseDoc(xmldesc)
+    name = doc.xpathNewContext().xpathEval("/domain/name")[0].content
+    return (name, xmldesc)
+
+def usage():
+   print 'Usage: %s domain.xml' % sys.argv[0]
+   print '       Check that the domain described by DOMAIN.XML is running'
+   print '       If the domain is not running, create it'
+   print '       DOMAIN.XML must be a XML description of the domain'
+   print '       in libvirt\'s XML format'
+
+if len(sys.argv) != 2:
+    usage()
+    sys.exit(2)
+
+(name, xmldesc) = read_domain(sys.argv[1])
+
+conn = libvirt.open(None)
+if conn is None:
+    print 'Failed to open connection to the hypervisor'
+    sys.exit(1)
+
+try:
+    dom = conn.lookupByName(name)
+except libvirt.libvirtError:
+    print "Starting domain %s ... " % name,
+    sys.stdout.flush()
+    dom = conn.createLinux(xmldesc, 0)
+    if dom is None:
+        print "failed"
+        sys.exit(1)
+    else:
+        print "done"

commit a66fcfaf2bd106649bc6d0b76e916ba80c962b80

examples/topology: define socketIds and siblingsIds as sets
socketIds and siblingsIds are declared as empty lists, filled by
list comprehensions, and later on re-used as sets.

They could be directly obtained from set comprehensions.

Fixes: 34aa32b ("Move python example programs into python/examples/ subdirectory")
Fixes: 3f4e32c ("examples: Invoke print("...") instead of print "..."")
Link: https://docs.python.org/3/tutorial/datastructures.html#sets
Signed-off-by: Ariel Otilibili <otilibil@eurecom.fr>
Signed-off-by: Michal Privoznik <mprivozn@redhat.com>
Reviewed-by: Michal Privoznik <mprivozn@redhat.com>

examples/topology.py
@@ -26,23 +26,18 @@ host = caps.getElementsByTagName('host')[0]
 cells = host.getElementsByTagName('cells')[0]
 total_cpus = cells.getElementsByTagName('cpu').length
 
-socketIds = []
-siblingsIds = []
-
-socketIds = [
+socketIds = {
     proc.getAttribute('socket_id')
     for proc in cells.getElementsByTagName('cpu')
-    if proc.getAttribute('socket_id') not in socketIds
-]
+}
 
-siblingsIds = [
+siblingsIds = {
     proc.getAttribute('siblings')
     for proc in cells.getElementsByTagName('cpu')
-    if proc.getAttribute('siblings') not in siblingsIds
-]
+}
 
 print("Host topology")
 print("NUMA nodes:", cells.getAttribute('num'))
-print("   Sockets:", len(set(socketIds)))
-print("     Cores:", len(set(siblingsIds)))
+print("   Sockets:", len(socketIds))
+print("     Cores:", len(siblingsIds))
 print("   Threads:", total_cpus)